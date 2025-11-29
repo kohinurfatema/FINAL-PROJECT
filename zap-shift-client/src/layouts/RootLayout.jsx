b1eac91b95a3e825c8b08080f81167f499b646e2
kohinurfatema <kohinurfatema35@gmail.com>
2025-11-29 06:33:50 -0800

    adding custom theme using daisyUI

--- zap-shift-client/src/layouts/RootLayout.jsx
+++ zap-shift-client/src/layouts/RootLayout.jsx
@@ -3,9 +3,10 @@ import { Outlet } from 'react-router';
 import Footer from '../pages/Shared/Footer/Footer';
 import NavBar from '../pages/Shared/Navbar/Navbar';
 
+
 const RootLayout = () => {
     return (
-        <div>
+        <div className='max-w-7xl mx-auto'>
             <NavBar></NavBar>
             <Outlet></Outlet>
             <Footer></Footer>
