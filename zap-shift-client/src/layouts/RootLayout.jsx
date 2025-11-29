52624b8dd4fa85671a929b2c8e8e8782b5c847d1
kohinurfatema <kohinurfatema35@gmail.com>
2025-11-29 06:05:26 -0800

    making NavBar succssesfully

--- zap-shift-client/src/layouts/RootLayout.jsx
+++ zap-shift-client/src/layouts/RootLayout.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Outlet } from 'react-router';
 import Footer from '../pages/Shared/Footer/Footer';
+import NavBar from '../pages/Shared/Navbar/Navbar';
 
 const RootLayout = () => {
     return (
         <div>
+            <NavBar></NavBar>
             <Outlet></Outlet>
             <Footer></Footer>
         </div>
