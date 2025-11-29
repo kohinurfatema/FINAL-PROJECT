c6c49f5cd7ea146b87040d945bba51056e93b512
kohinurfatema <kohinurfatema35@gmail.com>
2025-11-29 06:18:05 -0800

    making Logo.jsx file successfully

--- zap-shift-client/src/pages/Shared/Footer/Footer.jsx
+++ zap-shift-client/src/pages/Shared/Footer/Footer.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import Logo from '../../../components/Logo/Logo';
+
 
 const Footer = () => {
     return (
         <footer className="footer footer-horizontal text-secondary footer-center p-10">
             <aside>
-                 
+                 <Logo></Logo>
                 <p className="font-bold">
                     ACME Industries Ltd.
                     <br />
