92fbadf3cc93e9e475bf4863aa45f889e408efa4
kohinurfatema <kohinurfatema35@gmail.com>
2025-11-29 05:53:15 -0800

    footer added

---
+++ zap-shift-client/src/layouts/RootLayout.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Outlet } from 'react-router';
+import Footer from '../pages/Shared/Footer/Footer';
+
+const RootLayout = () => {
+    return (
+        <div>
+            <Outlet></Outlet>
+            <Footer></Footer>
+        </div>
+    );
+};
+
+export default RootLayout;
