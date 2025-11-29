52829a5479dc1edecf9be5e862facbaaac94afb8
kohinurfatema <kohinurfatema35@gmail.com>
2025-11-29 07:41:42 -0800

    added coverage page

--- zap-shift-client/src/routes/router.jsx
+++ zap-shift-client/src/routes/router.jsx
@@ -1,16 +1,21 @@
 import { createBrowserRouter } from "react-router";
 import RootLayout from "../layouts/RootLayout";
 import Home from "../pages/Home/Home/Home";
-
+import Coverage from "../pages/Coverage/Coverage";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     Component: RootLayout,
-    children : [
+    children: [
         {
-            index:true,
-            Component:Home
+            index: true, 
+            Component: Home
+        }, 
+        {
+            path: 'coverage',
+            Component: Coverage, 
+            loader: () => fetch('/serviceCenters.json').then(res => res.json())
         }
     ]
   },
